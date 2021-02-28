1ea8a737b2a345b0dea0afa6f806a4f1675305b6
AlexandrosVasileiou <alexandrosvasileiou@gmail.com>
2021-02-28 18:18:28 +0200

    Home page

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+// import React from "react";
+import "@fontsource/fira-mono" // Defaults to weight 400 with all styles included.
+// import "./src/normalize.css"; // normalize CSS across browsers
+// import "./src/style.css"; // custom CSS styles
+
+// export const wrapPageElement = ({ element, props }) => {
+//   return <Layout {...props}>{element}</Layout>;
+// };
